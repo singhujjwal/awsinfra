1b8f445d30ca2871dee81a8398bf42bc5e2772ad
Ujjwal Singh <singhujjwal@gmail.com>
2021-06-15 05:44:36 +0000

    Update table and bucket path

--- terraform/ec2/backend.tf
+++ terraform/ec2/backend.tf
@@ -2,7 +2,7 @@
 terraform {
   backend "s3" {
     bucket = "ujjwal-s3-bucket"
-    key    = "tfstate/vpc/vpc.tfstate"
+    key    = "tfstate/ec2/ec2.tfstate"
     region = "ap-south-1"
     dynamodb_table = "tfstate-table"
   }
---
+++ terraform/ec2/main.tf
@@ -0,0 +1,5 @@
+ "aws_vpc" "current_vpc" {
+  id = local.vpc_id
+}
+
+
